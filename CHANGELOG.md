0f7ed7303f859469da4896added8f65315ecf840
djipidi <djipidi.dev@gmail.com>
2021-03-15 14:32:32 +0000

    version 1.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,7 +2,7 @@
 
 All notable changes to this project will be documented in this file.
 
-## [Unreleased]
+## [1.1.1] - 2021-03-15
 
 ### Fixed
 
--- graphene_django_crud/__init__.py
+++ graphene_django_crud/__init__.py
@@ -5,7 +5,7 @@ from .types import DjangoGrapheneCRUD
 from .types import apply_where as where_input_to_Q
 
 
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 
 __all__ = (
     "__version__",
