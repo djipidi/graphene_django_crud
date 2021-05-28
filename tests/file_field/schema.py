3958c60bef46dd5fb08e8b6056d5d737760c82da
djipidi <djipidi.dev@gmail.com>
2021-05-28 08:54:36 +0000

    add fileField schema for test

---
+++ tests/file_field/schema.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from graphene_django_crud import DjangoCRUDObjectType
+import graphene
+from .models import *
+
+from graphene import relay
+
+class TestFileType(DjangoCRUDObjectType):
+    class Meta:
+        model = TestFile
+
+
+class Query(graphene.ObjectType):
+
+    test_file = TestFileType.ReadField()
+    test_files = TestFileType.BatchReadField()
+
+
+
+class Mutation(graphene.ObjectType):
+
+    test_file_create = TestFileType.CreateField()
+    test_file_update = TestFileType.UpdateField()
+    test_file_delete = TestFileType.DeleteField()
+
+
+
+
+class Subscription(graphene.ObjectType):
+
+    test_file_created = TestFileType.CreatedField()
+    test_file_updated = TestFileType.UpdatedField()
+    test_file_deleted = TestFileType.DeletedField()
