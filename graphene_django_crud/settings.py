ad7ef6d48e4acf97bafa37f8068d942b6efd574e
djipidi <djipidi.dev@gmail.com>
2021-06-08 07:12:35 +0000

    add settings manager

---
+++ graphene_django_crud/settings.py
@@ -0,0 +1,73 @@
+from django.conf import settings
+from django.test.signals import setting_changed
+from django.utils.module_loading import import_string
+
+DEFAULTS = {}
+
+IMPORT_STRINGS = ()
+
+
+def perform_import(value, setting_name):
+    if isinstance(value, str):
+        return import_from_string(value, setting_name)
+    if isinstance(value, (list, tuple)):
+        return [import_from_string(item, setting_name) for item in value]
+    return value
+
+
+def import_from_string(value, setting_name):
+    try:
+        return import_string(value)
+    except ImportError as e:
+        msg = (
+            f"Could not import `{value}` for GRAPHENE_DJANGO_CRUD setting `{setting_name}`."
+            f"{e.__class__.__name__}: {e}."
+        )
+        raise ImportError(msg)
+
+
+class GDCSettings:
+    def __init__(self, defaults, import_strings):
+        self.defaults = defaults
+        self.import_strings = import_strings
+        self._cached_attrs = set()
+
+    def __getattr__(self, attr):
+        if attr not in self.defaults:
+            raise AttributeError(f"Invalid setting: `{attr}`")
+
+        value = self.user_settings.get(attr, self.defaults[attr])
+
+        if attr in self.import_strings:
+            value = perform_import(value, attr)
+
+        self._cached_attrs.add(attr)
+        setattr(self, attr, value)
+        return value
+
+    @property
+    def user_settings(self):
+        if not hasattr(self, "_user_settings"):
+            self._user_settings = getattr(settings, "GRAPHENE_DJANGO_CRUD", {})
+        return self._user_settings
+
+    def reload(self):
+        for attr in self._cached_attrs:
+            delattr(self, attr)
+
+        self._cached_attrs.clear()
+
+        if hasattr(self, "_user_settings"):
+            delattr(self, "_user_settings")
+
+
+def reload_settings(*args, **kwargs):
+    setting = kwargs["setting"]
+
+    if setting == "GRAPHENE_DJANGO_CRUD":
+        gdc_settings.reload()
+
+
+setting_changed.connect(reload_settings)
+
+gdc_settings = GDCSettings(DEFAULTS, IMPORT_STRINGS)
