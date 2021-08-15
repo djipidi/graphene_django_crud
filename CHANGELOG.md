7e54d9a21e7c7cdcef7dd196dfc8312d0d3b810c
djipidi <djipidi.dev@gmail.com>
2021-08-15 09:39:40 +0100

    version 1.3.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,7 +2,7 @@
 
 All notable changes to this project will be documented in this file.
 
-## \[Unreleased\]
+## \[1.3.1\] 2021-08-15
 
 ### Fixed
 
--- graphene_django_crud/__init__.py
+++ graphene_django_crud/__init__.py
@@ -3,7 +3,7 @@ from .types import DjangoGrapheneCRUD, DjangoCRUDObjectType, resolver_hints
 from .utils import where_input_to_Q, order_by_input_to_args
 from .base_types import DefaultConnection, EmptyDefaultConnection
 
-__version__ = "1.3.0"
+__version__ = "1.3.1"
 
 __all__ = (
     "__version__",
